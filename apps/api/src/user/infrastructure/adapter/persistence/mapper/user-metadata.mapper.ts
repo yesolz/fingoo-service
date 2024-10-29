1fa4eb4e6aa61ea101b6a4f23bed5ab13d9d1d12
reindeer002 <lsj000424@gmail.com>
2024-10-29 17:48:07 +0000

    [MC-15] [api] feat: add user domain mapper

---
+++ apps/api/src/user/infrastructure/adapter/persistence/mapper/user-metadata.mapper.ts
@@ -0,0 +1,24 @@
+import { UserMetadataEntity } from '../entity/user-metadata.entity';
+import { UserMetadataDomain } from '../../../../domain/user-metadata.domain';
+import { GetUserResponseDto } from '../../../../api/dto/response/get-user.response.dto';
+import { UpdateUserResponseDto } from '../../../../api/dto/response/update-user.response.dto';
+
+export class UserMetadataMapper {
+  static mapDomainToGetDto(userMetadataDomain: UserMetadataDomain): GetUserResponseDto {
+    return new GetUserResponseDto(userMetadataDomain.email, userMetadataDomain.username);
+  }
+
+  static mapDomainToUpdateDto(userMetadataDomain: UserMetadataDomain): UpdateUserResponseDto {
+    return new UpdateUserResponseDto(userMetadataDomain.email, userMetadataDomain.username);
+  }
+
+  static mapEntityToDomain(userMetadataEntity: UserMetadataEntity): UserMetadataDomain {
+    return UserMetadataDomain.create(
+      userMetadataEntity.id,
+      userMetadataEntity.userId,
+      userMetadataEntity.email,
+      userMetadataEntity.username,
+      userMetadataEntity.posts,
+    );
+  }
+}
