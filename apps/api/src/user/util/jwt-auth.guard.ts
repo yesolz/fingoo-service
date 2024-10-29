146fd3e0c9a8bbb17ae3ef4cc71ab9fc681d6d54
reindeer002 <lsj000424@gmail.com>
2024-10-29 12:41:01 +0000

    [MC-15] [api] refactor: add supabaseConnection for using user data and include user data in request object

--- apps/api/src/user/util/jwt-auth.guard.ts
+++ apps/api/src/user/util/jwt-auth.guard.ts
@@ -1,24 +1,41 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 import { IS_PUBLIC_KEY } from './public.decorator';
 import { AuthGuard } from '@nestjs/passport';
+import { SupabaseConnection } from '../infrastructure/adapter/supabase/supabase.connection';
 
 @Injectable()
-export class CustomAuthGuard extends AuthGuard('jwt') {
-  async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  private reflector: Reflector = new Reflector();
 
-    const reflector: Reflector = new Reflector();
-    const isPublic = reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()]);
+  constructor(
+    @Inject(SupabaseConnection)
+    private supabaseConnection: SupabaseConnection,
+  ) {
+    super();
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
     if (isPublic) {
       return true;
     }
 
+    const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('[ERROR] 로그인하지 않은 사용자');
     }
+    const { data, error } = await this.supabaseConnection.connection.auth.getUser(token);
+    if (error) {
+      throw new UnauthorizedException('[ERROR] 유효하지 않은 토큰', error.message);
+    }
+
+    request.user = data.user;
     return true;
   }
 
