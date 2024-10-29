2d8c4a90ac1ea258d084dd39a0593a0c4be76456
reindeer002 <lsj000424@gmail.com>
2024-10-29 12:42:55 +0000

    [MC-15] [api] refactor: erase all of supabase code
    - Refactor logic to simply retrieve user data from the request object, as AuthGuard now adds user data to it

--- apps/api/src/user/util/login-user.decorator.ts
+++ apps/api/src/user/util/login-user.decorator.ts
@@ -1,18 +1,11 @@
 import { createParamDecorator, ExecutionContext, UnauthorizedException } from '@nestjs/common';
-import { SupabaseClient, User } from '@supabase/supabase-js';
-import * as process from 'process';
 
-export const LoginUser = createParamDecorator(async (_, ctx: ExecutionContext): Promise<User> => {
+export const LoginUser = createParamDecorator((_, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
 
-  const client: SupabaseClient = new SupabaseClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-  const [type, token] = request.headers.authorization?.split(' ') ?? [];
-  if (type !== 'Bearer') {
-    throw new UnauthorizedException(`[ERROR] 올바른 토큰 양식이 아닙니다(${type}).`);
+  if (!request.user) {
+    throw new UnauthorizedException('[ERROR] 로그인하지 않은 사용자');
   }
-  const userResponse = await client.auth.getUser(token);
-  if (!userResponse.data.user) {
-    throw new UnauthorizedException('[ERROR] 토큰이 만료되었습니다 다시 로그인해주세요.');
-  }
-  return userResponse.data.user;
+  // AuthGuard에서 user 데이터를 추가함
+  return request.user;
 });
