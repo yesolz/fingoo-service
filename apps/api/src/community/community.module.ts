48c898d610be688efddebf2db0b08c1779f1237d
reindeer002 <lsj000424@gmail.com>
2024-10-30 08:27:47 +0000

    [MC-15] [api] fix: fix module imports
    - Add CqrsModule, HttpModule, TypeOrmModule

--- apps/api/src/community/community.module.ts
+++ apps/api/src/community/community.module.ts
@@ -3,9 +3,21 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { PostController } from './api/post.controller';
 import { CreatePostCommandHandler } from './application/command/post/create-post/create-post.command.handler';
 import { CommunityPersistentAdapter } from './infrastructure/adapter/persistence/community.persistent.adapter';
+import { HttpModule } from '@nestjs/axios';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from './infrastructure/adapter/persistence/entity/post.entity';
 
 @Module({
-  imports: [CqrsModule],
+  imports: [
+    CqrsModule,
+    HttpModule.registerAsync({
+      useFactory: () => ({
+        timeout: 20000,
+        maxRedirects: 5,
+      }),
+    }),
+    TypeOrmModule.forFeature([PostEntity]),
+  ],
   controllers: [PostController],
   providers: [
     CreatePostCommandHandler,
