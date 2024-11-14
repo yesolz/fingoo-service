3cd33f6f6696b8d0cd7ad5d8e4f0c5bec1f2cea7
reindeer002 <lsj000424@gmail.com>
2024-11-14 07:53:44 +0000

    [MC-19] [api] feat: add data transfer object for get posts
    - GetPostsRequestDto: accept cursor value from the user for paginated post retrieval.
    - CursorPageMetadataDto: supplementary data used in cursor-based retrieval.
    - GetPostResponseDto: retrieve a single post, to be used in the implementation of the get-post feature.
    - GetPostsResponseDto: retrieve a paginated posts

---
+++ apps/api/src/community/api/dto/response/get-post.reponse.dto.ts
@@ -0,0 +1,69 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+import { CONTENT_LIMIT_RULE } from '../../../domain/rule/PostContentLengthShouldNotExceedLimit.rule';
+
+export class GetPostReponseDto {
+  @ApiProperty({
+    example: '1',
+    description: '게시글 PK',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly postId: string;
+
+  @ApiProperty({
+    example:
+      '“그래서 최저임금 얼마나 인상돼야 해?”는 저자기 이 책을 집필하면서 가장 많이 들은 질문이다. ' +
+      '최저임금 인상 결과에 따라 근로자와 고용자는 서로를 향한 전쟁과도 같은 분노를 쏟아낸다. ' +
+      '저자 김도경은 최저임금 인상 혹은 인하의 효과를 분석하는 것에 그치지 않고, 최저임금 전쟁의 본질과 논의의 요점, ' +
+      '더불어 딜레마에 빠진 최저임금 전쟁에 대한 해답을 쉽게 파악할 수 있게 책을 구성했다. ' +
+      '이 책은 단순한 최저임금 전쟁 설명서를 넘어서, 거대한 흐름에서 최저임금과 경제의 뼈대를 이해할 수 있는 기회가 될 것이다.',
+    description: '게시글 내용',
+  })
+  @MaxLength(CONTENT_LIMIT_RULE)
+  @IsString()
+  @IsNotEmpty()
+  readonly content: string;
+
+  @ApiProperty({
+    example: '김도경',
+    description: '게시글 작성자 이름',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly author: string;
+
+  @ApiProperty({
+    example: '100000',
+    description: '게시글 조회수',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly viewCount: string;
+
+  @ApiProperty({
+    example: '2024-10-02',
+    description: '생성 날짜',
+  })
+  @IsDate()
+  @IsString()
+  @IsNotEmpty()
+  readonly createdAt: string;
+
+  @ApiProperty({
+    example: '2024-10-02',
+    description: '수정 날짜',
+  })
+  @IsDate()
+  @IsString()
+  @IsNotEmpty()
+  readonly updatedAt: string;
+
+  constructor(postId: string, content: string, author: string, createdAt: string, updatedAt: string) {
+    this.postId = postId;
+    this.content = content;
+    this.author = author;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+  }
+}
