401f3cdd105e57a3d2604b2e8200ab6305bf430e
reindeer002 <lsj000424@gmail.com>
2024-11-14 22:49:17 +0000

    [MC-19] [api] test: add get posts by cursor api test to verify not throw exception
    - add delete and update test too

--- apps/api/src/community/test/unit-test/controller/post.controller.spec.ts
+++ apps/api/src/community/test/unit-test/controller/post.controller.spec.ts
@@ -3,6 +3,9 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreatePostRequestDto } from '../../../api/dto/request/create-post.request.dto';
 import { mockUser1 } from '../../../../user/test/data/mock-user.user1';
+import { UpdatePostRequestDto } from '../../../api/dto/request/update-post.request.dto';
+import { Response } from 'express';
+import { GetPostsRequestDto } from '../../../api/dto/request/get-posts.request.dto';
 
 describe('PostController', () => {
   let postController: PostController;
@@ -30,5 +33,27 @@ describe('PostController', () => {
       content: 'test content',
     };
     await expect(postController.createPost(createPostRequestDto, mockUser1)).resolves.not.toThrow();
+  });
+
+  it('update', async () => {
+    const updatePostRequestDto: UpdatePostRequestDto = {
+      content: 'test content',
+    };
+    await expect(postController.updatePost('123', updatePostRequestDto, mockUser1)).resolves.not.toThrow();
+  });
+
+  it('delete', async () => {
+    const response = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn(),
+    } as unknown as Response;
+    await expect(postController.deletePost('123', mockUser1, response)).resolves.not.toThrow();
+  });
+
+  it('get posts by cursor', async () => {
+    const getPostsRequestDto: GetPostsRequestDto = {
+      cursorId: 10,
+    };
+    await expect(postController.getPosts(getPostsRequestDto)).resolves.not.toThrow();
   });
 });
