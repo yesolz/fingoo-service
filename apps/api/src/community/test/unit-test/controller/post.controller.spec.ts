647fdfa573898d2e1f09efdc6c28e584f840c8a8
reindeer002 <lsj000424@gmail.com>
2024-11-22 06:46:30 +0000

    [MC-999] [api] refactor: implements mock object function

--- apps/api/src/community/test/unit-test/controller/post.controller.spec.ts
+++ apps/api/src/community/test/unit-test/controller/post.controller.spec.ts
@@ -2,8 +2,9 @@ import { PostController } from '../../../api/post.controller';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { CreatePostRequestDto } from '../../../api/dto/request/create-post.request.dto';
-import { mockUser1 } from '../../../../user/test/data/mock-user.user1';
+import { mockSession1, mockUser1 } from '../../../../user/test/data/mock-user.user1';
 import { SupabaseConnection } from '../../../../user/infrastructure/adapter/supabase/supabase.connection';
+import { mockSession2 } from '../../../../user/test/data/mock-user.user2';
 
 describe('PostController', () => {
   let postController: PostController;
@@ -25,9 +26,14 @@ describe('PostController', () => {
           useValue: {
             connection: {
               auth: {
-                getUser: jest.fn().mockResolvedValue({
-                  data: { user: { id: 'mock-user-id', email: 'mockuser@example.com' } },
-                  error: null,
+                getUser: jest.fn((token: string) => {
+                  if (token === mockSession1.access_token) {
+                    return Promise.resolve({ data: { user: mockUser1, session: mockSession1 }, error: null });
+                  } else if (token === mockSession2.access_token) {
+                    return Promise.resolve({ data: { user: null, session: null }, error: 'Invalid token' });
+                  } else {
+                    return Promise.resolve({ data: { user: null, session: null }, error: 'No token provided' });
+                  }
                 }),
               },
             },
